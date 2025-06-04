b3aae0227d82c6fce2da2840ccc01e64568db8fc
AkimaG82 <akima.gordon@testpro.io>
2025-06-03 22:16:21 -0400

    adding music to playlist

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework17 extends BaseTest {
+    @Test
+    public void addSongToPlayList() {
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.click();
+        emailField.sendKeys("akima.gordon@testpro.io");
+
+        WebElement PasswordField = driver.findElement(By.cssSelector("input[type='password']"));
+        PasswordField.click();
+        PasswordField.sendKeys("BabyboiAz1!");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+
+        WebElement search = driver.findElement(By.cssSelector("input[type='search']"));
+        Assert.assertTrue(search.isDisplayed());
+        search.click();
+        search.sendKeys("Episode 2");
+
+        WebElement viewAll = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
+        viewAll.click();
+
+        WebElement firstSong = driver.findElement(By.cssSelector("//table[@class='items' and @data-v-3eef485a]//tr[@class='song-item playing' and @draggable='true']//td[@class='title'][text()='Episode 2']"));
+        firstSong.click();
+
+        WebElement addTo = driver.findElement(By.cssSelector("button.btn-add-to"));
+        addTo.click();
+
+        WebElement playListName = driver.findElement(By.cssSelector("input[required='required']"));
+        playListName.click();
+        playListName.sendKeys("First Playlist");
+
+        WebElement save = driver.findElement(By.cssSelector("button[type='submit']"));
+    }
+}
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -10,15 +12,16 @@ public class LoginTests extends BaseTest {
     @Test
     public void navigationToLoginPage() {
 
-//      Added ChromeOptions argument below to fix websocket error
+   //   Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
-        WebDriver driver = new ChromeDriver(options);
+            WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        // TODO: Fix the URL if the test fails
-        String url = "htpps://qa.koel.app/";
+
+   //      TODO: Fix the URL if the test fails
+        String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
