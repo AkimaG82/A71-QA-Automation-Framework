c7fde216dcdecd37067dabff59078447393b4c74
AkimaG82 <akima.gordon@testpro.io>
2025-07-03 14:28:25 -0400

    hw18

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,2 +1,27 @@
-public class Homework19 {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+import java.util.UUID;
+
+import static org.bouncycastle.cms.RecipientId.password;
+
+public class Homework19 extends BaseTest {
+
+    public void deletePlaylist() {
+
+        @Test(dataProvider = "deletePlaylist", dataProviderClass = TestDataProvider.class)
+        public void negativeTesting (String email, String password) throws InterruptedException {
+            provideEmail(email);
+            providePassword(password);
+            clickSubmit();
+            Thread.sleep(2000);
+        }
+    }
 }
--- src/test/java/TestDataProvider.java
+++ src/test/java/TestDataProvider.java
@@ -3,14 +3,21 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestDataProvider extends BaseTest {
-    @DataProvider(name="IncorrectLoginData")
-    public static Object[][]negativeTestData(){
+    @DataProvider(name = "IncorrectLoginData")
+    public static Object[][] negativeTestData() {
         return new Object[][]{
-                {"Invalid@testpro.io","InvalidPassword"},
-                {"wron@testpro.io","wrpngPassword"},
-                {"Invalid@testpro.io","BabyboiAz1"},
-                {"","BabyboiAz1!"},
-                {"",""}
+                {"Invalid@testpro.io", "InvalidPassword"},
+                {"wron@testpro.io", "wrpngPassword"},
+                {"Invalid@testpro.io", "BabyboiAz1"},
+                {"", "BabyboiAz1!"},
+                {"", ""}
+        };
+    }
+    @DataProvider(name="deletePlaylist")
+    public static Object[][]positiveTestingData(){
+        return new Object[][]{
+                {"akima.gordon@testpro.io", "BabyboiAz1!"}
         };
     }
 }
+
